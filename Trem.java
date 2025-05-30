57bfc611e0a532f2593f0927c978e9e1249be50b
Marvio Rodrigues da silva <159482064+Marvio76@users.noreply.github.com>
2025-05-30 19:37:26 -0300

    Create Trem.java

---
+++ Trem.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Trem extends VeiculoTransporte {
+    public Trem(String nome, int capacidade) {
+
+        super(nome, capacidade);
+    }
+
+    @Override
+    public double calcularCustoViagem() {
+              // valor  //taxa
+        return 15.0 + (0.04 * capacidadePassageiros);
+    }
+    @Override
+    public void manutencao(){
+        if (getTotalViagens() == 10){
+            System.out.println( "o veiculo fez manutenção");
+        }
+    }
+}
